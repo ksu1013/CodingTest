6c03e25f50507293b89356b8c25d5bf45c85d32e
ksu1013 <124007756+ksu1013@users.noreply.github.com>
2026-01-28 17:09:58 +0900

    Add files via upload

---
+++ CodingTest/src/GmarketCT03.java
@@ -0,0 +1,14 @@
+package test.codingTest;
+
+class GmarketCT03 {
+    public static void main(String[] args) {
+//        String k = solution("...!@BaT#*..y.abcdefghijklm");
+        int[] nubmers = {1,1,1,1};
+        int target = 3;
+        int k = solution(nubmers,target);
+    }
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+}
